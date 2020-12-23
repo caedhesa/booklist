895e14e59c1fdff97ab1ab69af169aa1623f714e
Carlos Hernandez Santos <carlos@hernandezsantos.com>
2020-12-23 16:28:19 +0100

    second commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+function BookList() {
+  return (
+    <section>
+      <Book />
+    </section>
+  );
+}
+
+const Book = () => {
+  return (
+    <article>
+      <Image></Image>
+      <Title />
+      <Author />
+    </article>
+  );
+};
+const Image = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/81ySRJMhN5L._AC_UL480_FMwebp_QL65_.jpg"
+    alt=""
+  />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Title = () => <h1>Lo último que verán tus ojos</h1>;
+const Author = () => <h4>Isabel San Sebastián</h4>;
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
