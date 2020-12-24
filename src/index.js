cc3afe136a547e119929f6e98e7b6b18d4feab54
Carlos Hernandez Santos <carlos@hernandezsantos.com>
2020-12-24 02:17:01 +0100

    .

--- src/index.js
+++ src/index.js
@@ -5,24 +5,40 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 //setup vars
-const author = "Isabel San Sebastián";
-const title = "Lo último que verán tus ojos";
-const img = "https://m.media-amazon.com/images/I/81ySRJMhN5L._AC_UL320_.jpg";
+const firstBook = {
+  img: "https://m.media-amazon.com/images/I/81ySRJMhN5L._AC_UL320_.jpg",
+  title: "Lo último que verán tus ojos",
+  author: "Isabel San Sebastián",
+};
+const secondBook = {
+  img: "https://m.media-amazon.com/images/I/81M5pPsSGVL._AC_UL320_.jpg",
+  title: "Dime quién soy",
+  author: "Julia Navarro",
+};
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
   return (
     <article className="book">
-      <img src={img} alt="" />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
+      <img src={props.img} alt="" />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 };
