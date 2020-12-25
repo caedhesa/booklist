22002a5385a470a427aa2070094dc63212bfc3a6
Carlos Hernandez Santos <carlos@hernandezsantos.com>
2020-12-25 21:27:55 +0100

    .

--- src/index.js
+++ src/index.js
@@ -5,15 +5,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 import { books } from "./books";
-import Book from "./Book";
+import SpecificBook from "./Book";
+import { greeting } from "./testing/testing";
 
 //setup vars
 
 function BookList() {
+  console.log(greeting);
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book key={book.id} {...book}></Book>;
+        return <SpecificBook key={book.id} {...book}></SpecificBook>;
       })}
     </section>
   );
