ac35f9765bf785293049db83235a4fa0593801e5
Carlos Hernandez Santos <carlos@hernandezsantos.com>
2020-12-25 20:47:14 +0100

    .

--- src/index.js
+++ src/index.js
@@ -38,13 +38,20 @@ function BookList() {
   );
 }
 
-const Book = (props) => {
-  const { img, title, author } = props;
+const Book = ({ img, title, author }) => {
+  // attribute, eventHandler
+  // onClick, onMouseOver
+  const clickHandler = () => {
+    alert("hello world");
+  };
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        reference example
+      </button>
     </article>
   );
 };
