cfe4857ee1a7a54f5189a83a5b7b8c5d8a45169c
Carlos Hernandez Santos <carlos@hernandezsantos.com>
2020-12-25 21:08:34 +0100

    .

--- src/index.js
+++ src/index.js
@@ -51,7 +51,12 @@ const Book = ({ img, title, author }) => {
   };
 
   return (
-    <article className="book">
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
       <img src={img} alt="" />
       <h1 onClick={() => console.log(title)}>{title}</h1>
       <h4>{author}</h4>
